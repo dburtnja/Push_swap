a3eea84dee3af64a4340ac66826e81640aa7dc21
dburtnja <dostup.te@gmail.com>
2017-02-26 13:29:42 +0200

    swap if only == 2

--- push_swap_dir/q_sort_1.c
+++ push_swap_dir/q_sort_1.c
@@ -17,8 +17,8 @@ void	sort_small(t_doub_lst *a, t_doub_lst *b)
 	int		a_true;
 	int 	b_true;
 
-	a_true = (a && a->next && a->nbr > a->next->nbr && a->size > 1);
-	b_true = (b && b->next && b->nbr < b->next->nbr && b->size > 1);
+	a_true = (a && a->next && a->nbr > a->next->nbr && a->size == 2);
+	b_true = (b && b->next && b->nbr < b->next->nbr && b->size == 2);
 	if (a_true && b_true)
 	{
 		ps_apply_to_both(&a, &b, &ps_swap_stack);

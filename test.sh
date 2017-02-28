9cf501051beb32f966f559b1b80649d903596200
dburtnja <dostup.te@gmail.com>
2017-02-28 10:19:31 +0200

    add test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+echo my = 33
+ARG="7 8 9 4 5 6 1 2 3 10"; ./push_swap $ARG | wc -l
+ARG="7 8 9 4 5 6 1 2 3 10"; ./push_swap $ARG | ./checker -c -v $ARG
+
+echo my = 22
+ARG="7 8 9 4 5 6 1 2 3"; ./push_swap $ARG | wc -l
+ARG="7 8 9 4 5 6 1 2 3"; ./push_swap $ARG | ./checker $ARG
+
+echo my = 22
+ARG="8 9 4 6 1 2 3"; ./push_swap $ARG | wc -l
+./push_swap $ARG | ./checker $ARG
+
+echo my = KO
+ARG="7 8 9 4 5 6 1 2 3 10 11 12"; ./push_swap $ARG | wc -l
+./push_swap $ARG | ./checker $ARG
+
+echo my = KO 77
+ARG="2147483647 -2147483648 9 4 6 1 2 3 0 -22 36 22 -36 -1 -256"; ./push_swap $ARG | wc -l
+./push_swap $ARG | ./checker $ARG

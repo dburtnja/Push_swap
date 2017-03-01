fbea604697297f4aef2ea57b5f20347e1ca4198a
Деніс <denys@192.168.174.128.local.ipnet.ua>
2017-03-01 10:45:47 +0200

    test.sh

--- test.sh
+++ test.sh
@@ -16,4 +16,8 @@ ARG="7 8 9 4 5 6 1 2 3 10 11 12"; ./push_swap $ARG | wc -l
 
 echo my = KO 77
 ARG="2147483647 -2147483648 9 4 6 1 2 3 0 -22 36 22 -36 -1 -256"; ./push_swap $ARG | wc -l
-./push_swap $ARG | ./checker -c $ARG
+./push_swap $ARG | ./checker -c -v $ARG
+
+echo my = 
+ARG="256 -257 9 4 6 1 2 3 0 -22 36 22 -36 -1 -256"; ./push_swap $ARG | wc -l
+./push_swap $ARG | ./checker -c -v $ARG

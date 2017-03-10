060083a39303c89cd8fcf5958f2276ce26fe829b
dburtnja <dostup.te@gmail.com>
2017-03-10 12:08:01 +0200

    try add sort three

--- push_swap_dir/sort_a_part.c
+++ push_swap_dir/sort_a_part.c
@@ -89,8 +89,8 @@ void	sort_a_part(int *a, int *b, char **str, int *s)
 		if (find_instructions_a(a, b, str, &midd[0]))
 			break ;
 	}
-	size[0] = *a;
 	size[1] = *b - size[1] > 0 ? *b - size[1] : *b;
+	size[0] = s[0] - size[1];
 //	if (a[0] == 3)
 //		sort_three(a, b, str);
 	while (midd[1] && !midd[2])
--- push_swap_dir/sort_three.c
+++ push_swap_dir/sort_three.c
@@ -40,9 +40,9 @@ void	sort_three_a_full(int *a, int *b, int *s, char **str)
 {
 	int 	bul[2];
 
+	ps_swap_both(a, b, str, 't');
 	bul[0] = s[0] == 3 && check_if_sort_a(a, s[0]) == 0;
 	bul[1] = s[1] == 3 && check_if_sort_b(b, s[1]) == 0;
-	ps_swap_both(a, b, str, 't');
 	rot_a_and_or_b(a, b, &bul[0], str);
 	ps_swap_both(a, b, str, 't');
 	rev_rot_a_and_or_b(a, b, &bul[0], str);
